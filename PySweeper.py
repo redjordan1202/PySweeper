0d2fe3e686f95a89633854448cb10aa769cf1d31
Jordan Del Pilar <redjordan123@gmail.com>
2022-04-26 21:27:53 -0700

    Inital Commit

---
+++ PySweeper.py
@@ -0,0 +1,15 @@
+#PySweeper -  Pygame based Minesweeper Clone
+
+#Draw grid on screen
+
+#Remove Square on click
+
+#Place Mines
+
+#Gameover if Mine clicked on
+
+#Display number on clicked Squares
+
+#Place Flag and protect square
+
+#Win once all non-mine squares are gone
---
+++ vars.py
@@ -0,0 +1,8 @@
+#Variable file
+import pygame
+
+#Tile Size Variable
+
+#Number of Mines per difficult level
+
+#Grid size per difficulty level
