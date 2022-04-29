3690b5254ceab6c5dff2cf3cd25f2f68d884ca62
redjordan1202 <75704668+redjordan1202@users.noreply.github.com>
2022-04-29 11:35:05 -0700

    feat(status bar): Added basic status bar
    -Added in the status bar at the top of the window.
    -Also changed around the look of the window itself, and the buttons in the grid to make them look a bit less awkward and white.
    -Generally cleaned up the code, added more comments etc.
    
    TODO
    - Reorder the widget declaration and drawing.
    - Contuine working down the outline of the program

--- PySweeper.py
+++ PySweeper.py
@@ -8,13 +8,60 @@ class App:
     def __init__(self, master):
         self.master = master
         master.title("PySweeper")
+        root.configure(bg=DARK_GREY)
 
-        self.frm_grid = Frame(master=master)
+        #creating a blank image to allow pixel sizing on buttons
+        self.blank_img = PhotoImage()
+
+
+        #Draw status bar
+        self.frm_status = Frame(master=master,
+            height=60,
+            width=250,
+            bg=DARK_GREY,
+            relief='sunken',
+            borderwidth=5
+        )
+        self.btn_reset = Button(master=self.frm_status,
+            image=self.blank_img,
+            width=25,
+            height=25,
+            bg='yellow'
+        )
+        self.ent_mine_count = Entry(master=self.frm_status,
+            state='disabled',
+            font=('Arial', 24),
+            disabledbackground='black'
+        )
+        self.ent_score = Entry(master=self.frm_status,
+            state='disabled',
+            font=('Arial', 24),
+            disabledbackground='black'
+        )
+
+        self.frm_status.pack(pady=10)
+        self.frm_status.grid_propagate(0)
+        self.frm_status.grid_columnconfigure(0, weight=1)
+        self.frm_status.grid_columnconfigure(2, weight=1)
+        self.frm_status.grid_rowconfigure(0, weight=1)
+        self.frm_status.grid_rowconfigure(2, weight=1)
+        self.ent_mine_count.grid(column=0, row=1, padx=5)
+        self.btn_reset.grid(column=1, row=1)
+        self.ent_score.grid(column=2, row=1, padx=5)
+
+
+        #Drawing Frame around button grid
+        self.frm_grid = Frame(master=master, 
+            bg=DARK_GREY,
+            relief='sunken',
+            borderwidth=5
+        )
         self.frm_grid.pack()
         self.frm_grid.pack_propagate(0)
 
-        self.blank_img = PhotoImage()
+        
         self.draw_grid()
+        
 
 #Draw grid of buttons
     def draw_grid(self):
@@ -22,18 +69,22 @@ class App:
         for y in range(0, EASY_ROWS, 1):
             for x in range(0, EASY_COLS, 1):
                 self.button = Button(master=self.frm_grid, 
-                    bg='white',
+                    bg=DARK_GREY,
                     width=TILE_SIZE,
                     height=TILE_SIZE,
                     image=self.blank_img,
                     relief='raised',
-                    borderwidth=20
+                    borderwidth=5,
                 )
-                self.button.grid(column=x, row=y)
+                self.button.grid(column=x, 
+                    row=y,
+                    padx=2,
+                    pady=2
+                )
 
 #Dynamic Window Size based on size of grid
 
-#Draw status bar, adjust window to fit
+
 
 #Remove Square on click
 
@@ -51,6 +102,6 @@ class App:
 if __name__ == "__main__":
     root = Tk()
     #Using this window size until we have function for window size
-    root.geometry('500x500')
+    root.geometry('300x400')
     mainApp = App(root)
     root.mainloop()
--- vars.py
+++ vars.py
@@ -1,7 +1,10 @@
 #Tile Sizes
-TILE_SIZE = 40
+TILE_SIZE = 10
 
 
 #Difficulty Levels
 EASY_ROWS = 9
 EASY_COLS = 9
+
+#colors in hex
+DARK_GREY = '#7e7e7e'
